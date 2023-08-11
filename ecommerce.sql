c6b19cfab6a76d9743bbc6681fb218924e0159a1
ISnake <allanalr91@gmail.com>
2023-08-11 17:04:50 -0300

    Criando tabela relacional de n pra n

--- ecommerce.sql
+++ ecommerce.sql
@@ -39,9 +39,69 @@ create table orders(
     idOrdersClient int,
     ordersStatus enum('Cancelado', 'Confirmado', 'Em processamento'),
     ordersDescription varchar(255),
-    frete float default 10,
+    freight float default 10,
     paymentCash bool default false,
+    idPayment int,
     constraint fk_orders_client foreign key (idOrdersClient) references clients(idClient) 
 );
 
 -- criar tabela Pagamento
+
+create table payment(
+	idPayment int auto_increment,
+    idPaymentOrders int,
+    idClient int,
+    typePayment enum('Cartão','Pix', 'PagSeguro'),
+    period_buy datetime,
+    limitAvailable float,
+    primary key (idClient, idPayment),
+    constraint fk_pedido foreign key (idPaymentOrders) references orders(idOrders),
+    constraint fk_client foreign key (idClient) references clients(idClient)
+);
+
+-- criar tabela Estoque
+create table stock(
+	idStock int auto_increment primary key,
+    stockLocal varchar (255),
+    quantity int default 0
+);
+
+-- criar tabela fornecedor 
+create table supplier(
+	idSupplier int auto_increment primary key,
+    socialName varchar (45) not null,
+    CNPJ char(15) not null,
+    contact char(11) not null,
+    constraint unique_cnpj_supplier unique (CNPJ)
+);
+
+-- criar tabela Vendedor
+create table seller(
+	idSeller int auto_increment primary key,
+    socialName varchar(45) not null unique,
+    sellerLocal varchar(45),
+    fantasyName varchar(45),
+	CNPJ char(15),
+    CPF char(9),
+    contact char(11) not null,
+    constraint unique_cnpj_seller unique (CNPJ),
+    constraint unique_cpf_seller unique (CPF)
+);	
+
+-- criar tabela Disponibilizando um produto
+create table productHave (
+	productIdProduct int,
+    stockIdStock int,
+    constraint fk_productSeller foreign key (productIdProduct) references product (idProduct)
+);
+
+-- criar tabela Produtos por vendedor
+create table productsSeller (
+	idProductSeller int,
+    idProduct int,
+    quantity int default 1,
+    primary key (idProductSeller, idSeller),
+    constraint fk_product_seller foreign key (idProductSeller) references seller(idSeller),
+    constraint fk_product_product foreign key (idProduct) references product(idProduct)
+    
+);
